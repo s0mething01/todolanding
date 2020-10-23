b3ccdc58e2bf2dfd10ace0fcd9f90916e2b56473
Mikołaj <miki70707@interia.pl>
2020-10-23 22:00:12 +0200

    add adventeges component to document

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroHeader from 'components/HeroHeader/HeroHeader';
 import Intro from 'components/Intro/Intro';
+import Adventages from 'components/Adventages/Adventages';
 
 const IndexPage = () => (
   <>
     <HeroHeader />
     <Intro />
+    <Adventages />
   </>
 );
 
