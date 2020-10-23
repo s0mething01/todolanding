99786312c342441697f4548aecaac4ea47cd0e00
Mikołaj <miki70707@interia.pl>
2020-10-23 21:58:14 +0200

    updated section to show logo ang bgc when needed

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Section = ({ children, bgcolor }) => {
-  return <SectionWrapper bgcolor={bgcolor}>{children}</SectionWrapper>;
+import Logo from 'assets/logo/logo-white.inline.svg';
+import getGradient from 'utils/getGradient';
+
+const Section = ({ children, bgcolor, isLogo }) => {
+  return (
+    <SectionWrapper bgcolor={bgcolor}>
+      <div>
+        {children}
+        {isLogo && <Logo className="logo" />}
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Section;
 
 const SectionWrapper = styled.section`
-  background-color: ${({ bgcolor }) => bgcolor};
-  max-width: 1024px;
-  margin: 50px auto;
+  background-image: ${({ bgcolor, theme }) => getGradient(theme, bgcolor)};
+  position: relative;
+  margin: 25px 0;
+  & > div {
+    max-width: 1024px;
+    margin: 0 auto;
+    padding: 25px 0;
+  }
+  .logo {
+    position: absolute;
+    bottom: 10px;
+    right: 30px;
+    height: 30px;
+    width: auto;
+  }
 `;
