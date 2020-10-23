cbad41d437d44e11f626bde1418dd98832c309d4
Mikołaj <miki70707@interia.pl>
2020-10-23 21:59:33 +0200

    create basic structure for adventeges component

---
+++ src/components/Adventages/Adventages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Section from 'components/Section/Section';
+
+const Adventages = () => {
+  return (
+    <Section bgcolor="primary">
+      <p>fsdfsd</p>
+    </Section>
+  );
+};
+
+export default Adventages;
