cc859eb7f02af472b34a212ff0ff1dac7adca7d9
Mikołaj <miki70707@interia.pl>
2020-10-25 19:42:30 +0100

    add white option to paragraph and heading components

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Heading = styled.h2`
-  color: ${({ theme }) => theme.colors.secondary};
+  color: ${({ theme, isWhite }) => (isWhite ? theme.colors.white : theme.colors.secondary)};
   font-size: ${({ theme }) => theme.font.L};
   text-align: ${({ isCentered }) => (isCentered ? 'center' : 'left')};
 `;
--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Paragraph = styled.p`
-  color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme, isWhite }) => (isWhite ? theme.colors.white : theme.colors.primary)};
   font-size: ${({ theme }) => theme.font.M};
   text-align: ${({ isCentered }) => (isCentered ? 'center' : 'left')};
   line-height: ${({ theme }) => theme.font.lineHeightM};
