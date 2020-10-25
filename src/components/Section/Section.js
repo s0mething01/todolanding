13cab7d1994e17b9f3d018e91d26b2fe55db0e8d
Mikołaj <miki70707@interia.pl>
2020-10-25 02:10:26 +0200

    increase margin bottom of section component

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -20,7 +20,7 @@ export default Section;
 const SectionWrapper = styled.section`
   background-image: ${({ bgcolor, theme }) => getGradient(theme, bgcolor)};
   position: relative;
-  margin: 25px 0;
+  margin: 25px 0 125px;
   & > div {
     max-width: 1024px;
     margin: 0 auto;
