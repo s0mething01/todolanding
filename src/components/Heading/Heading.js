e917c20786240ff8dfb443e6a83102ae03fa75c2
Mikołaj <miki70707@interia.pl>
2020-10-25 02:09:42 +0200

    updated heading and paragraph elements

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Heading = styled.h2`
   color: ${({ theme }) => theme.colors.secondary};
-  font-size: ${({ theme }) => theme.font.l};
+  font-size: ${({ theme }) => theme.font.L};
   text-align: ${({ isCentered }) => (isCentered ? 'center' : 'left')};
 `;
 
--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Paragraph = styled.p`
   color: ${({ theme }) => theme.colors.primary};
-  font-size: ${({ theme }) => theme.font.m};
+  font-size: ${({ theme }) => theme.font.M};
   text-align: ${({ isCentered }) => (isCentered ? 'center' : 'left')};
   line-height: 25px;
   letter-spacing: -0.5px;
