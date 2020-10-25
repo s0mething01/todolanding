384d46530a14f8d137481cf4f5fb3b7f69f83d0c
Mikołaj <miki70707@interia.pl>
2020-10-25 02:10:57 +0200

    add adventages and adventages card components

--- src/components/Adventages/Adventages.js
+++ src/components/Adventages/Adventages.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import RulesCardSvg from 'assets/svg/RulesCard.inline.svg';
+import HappinessCardSvg from 'assets/svg/HappinessCard.inline.svg';
+import TimeCardSvg from 'assets/svg/TimeCard.inline.svg';
+
 import Section from 'components/Section/Section';
+import AdventagesCard from './AdventagesCard';
+
+const data = [
+  {
+    title: 'Twoje zasady',
+    text:
+      'With ToDo app you will dive into the world of done house chores.With ToDo app you will dive into the world of done house chores.With ToDo app you will dive into the world of done house chores.',
+    icon: <RulesCardSvg />,
+  },
+  {
+    title: 'Twój czas',
+    text:
+      'With ToDo app you will dive into the world of done house chores.With ToDo app you will dive into the world of done house chores.With ToDo app you will dive into the world of done house chores.',
+    icon: <TimeCardSvg />,
+  },
+  {
+    title: 'Twoje szczęście',
+    text:
+      'With ToDo app you will dive into the world of done house chores.With ToDo app you will dive into the world of done house chores.With ToDo app you will dive into the world of done house chores.',
+    icon: <HappinessCardSvg />,
+  },
+];
 
 const Adventages = () => {
   return (
     <Section bgcolor="primary">
-      <p>fsdfsd</p>
+      <CardWrapper>
+        {data.map(({ title, text, icon }) => (
+          <AdventagesCard key={title}>
+            <h5>{title}</h5>
+            <p>{text}</p>
+            {icon}
+          </AdventagesCard>
+        ))}
+      </CardWrapper>
     </Section>
   );
 };
 
 export default Adventages;
+
+const CardWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
---
+++ src/components/Adventages/AdventagesCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AdventagesCard = ({ children }) => {
+  return <CardWrapper>{children}</CardWrapper>;
+};
+
+export default AdventagesCard;
+
+const CardWrapper = styled.article`
+  width: 28%;
+  position: relative;
+  padding: 40px 0;
+  margin: 30px 0;
+  h5 {
+    font-size: ${({ theme }) => theme.font.L};
+    color: ${({ theme }) => theme.colors.white};
+    z-index: 2;
+    position: relative;
+    font-weight: ${({ theme }) => theme.font.bold};
+  }
+  p {
+    font-size: ${({ theme }) => theme.font.M};
+    color: ${({ theme }) => theme.colors.white};
+    line-height: 25px;
+    letter-spacing: -0.5px;
+    z-index: 2;
+    position: relative;
+    margin-top: 20px;
+    font-weight: ${({ theme }) => theme.font.light};
+  }
+  svg {
+    position: absolute;
+    height: 80%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+  }
+`;
