3fdb809f1ec78c719fb9ffd67b5277c93a1d9ec9
Mikołaj <miki70707@interia.pl>
2020-10-26 00:54:23 +0100

    add 4 more sections to document

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,11 @@ import HeroHeader from 'components/HeroHeader/HeroHeader';
 import Intro from 'components/Intro/Intro';
 import Adventages from 'components/Adventages/Adventages';
 import Pricing from 'components/Pricing/Pricing';
+import AdditionalInfo from 'components/AdditionalInfo/AdditionalInfo';
+import Knowledge from 'components/Knowledge/Knowledge';
+import Features from 'components/Features/Features';
+
+import Platforms from '../components/Platforms/Platforms';
 
 const IndexPage = () => (
   <>
@@ -10,6 +15,10 @@ const IndexPage = () => (
     <Intro />
     <Adventages />
     <Pricing />
+    <Platforms />
+    <AdditionalInfo />
+    <Knowledge />
+    <Features />
   </>
 );
 
