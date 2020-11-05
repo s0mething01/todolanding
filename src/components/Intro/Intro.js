27bf9942030d1aa34d9ac1a3040c17864dc068ec
Mikołaj <miki70707@interia.pl>
2020-11-06 00:10:06 +0100

    disable content animations

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
+// import React, { useRef, useEffect } from 'react';
 import styled from 'styled-components';
-import gsap from 'gsap';
+// import gsap from 'gsap';
 
 import Section from 'components/Section/Section';
 import Heading from 'components/Heading/Heading';
@@ -8,18 +9,19 @@ import Paragraph from 'components/Paragraph/Paragraph';
 import Button from 'components/Button/Button';
 
 const Intro = () => {
-  const section = useRef(null);
+  // const section = useRef(null);
 
-  useEffect(() => {
-    const sectionEl = section.current;
+  // useEffect(() => {
+  //   const sectionEl = section.current;
 
-    gsap.set(sectionEl, { opacity: 0, y: -200 });
-    gsap.to(sectionEl, { opacity: 1, y: 0, delay: 0.3, duration: 0.6 });
-  }, []);
+  //   gsap.set(sectionEl, { opacity: 0, y: -200 });
+  //   gsap.to(sectionEl, { opacity: 1, y: 0, delay: 0.3, duration: 0.6 });
+  // }, []);
 
   return (
     <Section>
-      <IntroWrapper ref={section}>
+      {/* <IntroWrapper ref={section}> */}
+      <IntroWrapper>
         <Heading isCentered>
           Czym jest aplikacja ToDo <br /> I dla kogo będzie się idealnie sprawdzać
         </Heading>
